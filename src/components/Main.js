39a10d60752af8067a1ef6b50092333352de49ec
AzimjonKing <azimjonjalilov1301@gmail.ru>
2022-07-26 18:09:12 +0300

    front page boldi

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../components/Main.css"
 import { Navbar } from "../pages/navbar/Navbar";
+import { Front } from "../pages/front/Front";
 
 export function Main (){
     return(
         <div className="main">
             <Navbar/>
+            <Front/>
             saad
             
         </div>
--- src/pages/front/Front.js
+++ src/pages/front/Front.js
@@ -1,7 +1,112 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
+import "./front.css"
+import 'primeicons/primeicons.css';
 
-export function Front(){
-    return(
-        <div></div>
-    )
+import image1 from "../../assets/front/image1.png";
+import image2 from "../../assets/front/image2.png";
+import slideImg1 from "../../assets/front/slideimg1.png";
+import slideImg2 from "../../assets/front/slideimg2.png";
+import slideImg3 from "../../assets/front/slideimg3.png";
+import slideImg4 from "../../assets/front/slideimg4.png";
+
+export function Front() {
+
+    const [ seeMore, setSeeMore ] = useState(true);
+    const moreBtn = () => {
+        setSeeMore(!seeMore)
+        console.log(seeMore);
+    }
+
+
+    return (
+        <div className="home">
+
+            <div className="section1">
+                <div className="slides-container">
+                    <div className="slide active bcg1">
+                        <h1>WELCOME TO</h1>
+                        <p>HNG HOTEL</p>
+                        <span>Your comfort, our priority</span>
+                    </div>
+
+                    <div className="slide bcg2">
+                        <h1>WELCOME TO</h1>
+                        <p>HNG HOTEL</p>
+                        <span>Your comfort, our priority</span>
+                    </div>
+
+                    <div className="slide bcg3">
+                        <h1>WELCOME TO</h1>
+                        <p>HNG HOTEL</p>
+                        <span>Your comfort, our priority</span>
+                    </div>
+                </div>
+                <div id="next-slide"><i className="pi pi-angle-right"></i></div>
+                <div id="prev-slide"><i className="pi pi-angle-left"></i></div>
+            </div>
+
+            <div className="section2">
+                <div className="left">
+                    <p>HNG HOTEL</p>
+                    <span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.
+                    </span>
+                    <span>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.
+                    </span>
+                </div>
+
+                <div className="right">
+
+                    <div className="img">
+                        <img src={image1} alt="" />
+                    </div>
+                    <div className="img">
+                        <img src={image2} alt="" />
+                    </div>
+
+                </div>
+            </div>
+            
+            <div className="section3">
+                <button className='morePhotos' onClick={moreBtn}>More Photos</button>
+                <div className={seeMore ? 'box-container' : 'box-container active'} id="photo-container">
+                    <div className="box">
+                        <img src={slideImg1} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg2} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg3} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg4} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg1} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg2} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg3} alt="" />
+                    </div>
+                    <div className="box">
+                        <img src={slideImg4} alt="" />
+                    </div>
+                </div>
+                <div className="box-position">
+                    <p>Book A Room</p>
+                    <span>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.
+                    </span>
+                    <button>Book A Room</button>
+                </div>
+            </div>
+
+
+
+        </div>
+    );
 }
